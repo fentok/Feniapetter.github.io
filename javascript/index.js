1eb28ba454c5bc986a994f1aa67631235e93c497
fentok <chaos943@gmail.com>
2019-09-09 15:52:18 +0100

    tweet

--- javascript/index.js
+++ javascript/index.js
@@ -7,10 +7,26 @@ counter.addEventListener(updateResetButton);
 var fenia = document.getElementById("feniabody");
 
 // フェニアをクリック
-function feniaclick(){
+function feniaclick() {
   counter.add();
   counter.update();
-  alert("クリックしました!");
 }
 
 fenia.onclick = feniaclick();
+
+
+function setTweetButton(text) {
+  $('#tweet-area').empty(); //既存のボタン消す
+  // htmlでスクリプトを読んでるからtwttがエラーなく呼べる
+  // オプションは公式よんで。
+  twttr.widgets.createShareButton(
+    "",
+    document.getElementById("tweet-area"),
+    {
+      size: "large", //ボタンはでかく
+      text: text, // 狙ったテキスト
+      hashtags: "ハッシュタグ", // ハッシュタグ
+      url: "//url" // URL
+    }
+  );
+}
